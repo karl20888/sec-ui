3145d9d346c032b249c5febdbb1b83599e36db6f
杨家华 <karl20888@live.cn>
2022-11-21 13:49:17 +0800

    'docs'

--- doc/src/views/Home.vue
+++ doc/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <sec-block class="doc-block">
     <h1>SecUI</h1>
-    <p>基于 <a href="https://element.eleme.cn/#/zh-CN">Element UI</a> 封装的一套适用于后台管理界面的 UI 组件库，功能基本完成，相关文档仍在补充中。</p>
+    <p>基于 <a href="https://element.eleme.cn/#/zh-CN">Element UI</a> 封装的一套适用于快速搭建后台管理界面的 UI 组件库，功能基本完成，相关文档仍在补充中。</p>
     <p>一个个组件整理并优化的，真的很辛苦，求您慷慨解囊打赏一下吧^_^</p>
     <p><img src="/img/q.jpg" alt="求打赏"></p>
   </sec-block>
